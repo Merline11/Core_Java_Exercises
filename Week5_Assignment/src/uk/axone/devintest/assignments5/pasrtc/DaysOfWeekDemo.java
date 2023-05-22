d6a9765d073a7b7571ea2fd613abf2ec983391c1
Merline <merlinemartin@gmail.com>
2023-05-22 10:06:49 +0100

    fixed error file

--- Week5_Assignment/src/uk/axone/devintest/assignments5/pasrtc/DaysOfWeekDemo.java
+++ Week5_Assignment/src/uk/axone/devintest/assignments5/pasrtc/DaysOfWeekDemo.java
@@ -5,10 +5,10 @@ public class DaysOfWeekDemo {
     public static void main(String[] args)  {
         try {
             DaysOfWeek wed = new DaysOfWeek("WED");
-            System.out.println(wed.getDay());
+            //System.out.println(wed.getDay());
 
             DaysOfWeek uk = new DaysOfWeek("UK");
-            System.out.println(uk.getDay());
+            //System.out.println(uk.getDay());
 
         } catch (InvalidDayException ide) {
             System.out.println(ide.getMessage());
