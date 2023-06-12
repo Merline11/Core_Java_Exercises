3f8af01284bc95f9002b87fa509ba01fd0768f27
Merline <merlinemartin@gmail.com>
2023-06-12 15:14:44 +0100

    added invaliddataException

--- Week7_Assignment/src/main/java/uk/axone/devintest/shoppingcart/exception/InvalidDataException.java
+++
@@ -1,8 +0,0 @@
-package uk.axone.devintest.shoppingcart.exception;
-
-public class InvalidDataException extends Exception {
-
-    public InvalidDataException(String message){
-        super(message);
-    }
-}
